d25608e30aa36e163ec960504dafd63a9370f736
jaybee <jubril_tunde@yahoo.com>
2025-08-02 21:45:34 +0100

    fix faq

--- src/components/Pages/Faqs.jsx
+++ src/components/Pages/Faqs.jsx
@@ -3,10 +3,10 @@ import Accordions from '../CommonComponents/Accordion'
 
 const Faqs = () => {
   return (
-   
-    <div className=" px-44 py-12 ">
-        <h1 className='text-3xl font-bold text-center mb-16'>Frequently Asked Questions</h1>
-        <Accordions />
+
+    <div className=" p-8 md:px-44 md:py-12 ">
+      <h1 className='text-xl md:text-3xl font-bold text-center mb-10 md:mb-16'>Frequently Asked Questions</h1>
+      <Accordions />
     </div>
   )
 }
